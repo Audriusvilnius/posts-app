ecc7b100dfb88b701266ecc4ac660a3cfdce5250
Audrius <audrius.ivko@gmail.com>
2024-09-25 20:51:18 +0300

    Refactor users index view to improve layout and functionality

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,61 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Users Management</h2>
+    <div class="container">
+        <div class="justify-content-center">
+            <div class="row">
+                <div class="col-lg-12 justify-content-between d-flex align-content-center align-items-center py-3">
+                    <h2>{{ __('Users') }}</h2>
+                    <a class="btn btn-success btn-sm mb-2 px-4" href="{{ route('users.create') }}"><i
+                            class="fa fa-plus me-2"></i>
+                        {{ __('Create New User') }}</a>
+                </div>
             </div>
-            <div class="pull-right">
-                <a class="btn btn-success mb-2" href="{{ route('users.create') }}"><i class="fa fa-plus"></i> Create New
-                    User</a>
+            @session('success')
+                <div class="alert alert-success" role="alert">
+                    {{ $value }}
+                </div>
+            @endsession
+            <div class="col-md-12">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-sm table-hover">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Roles</th>
+                                    <th width="240px" class=" align-content-center align-items-center">Action</th>
+                                </tr>
+                                @forelse ($data as $key => $user)
+                                    <tr>
+                                        <th scope="row">{{ ++$i }}</th>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>
+                                            @if (!empty($user->getRoleNames()))
+                                                @foreach ($user->getRoleNames() as $v)
+                                                    <label class="badge bg-success">{{ $v }}</label>
+                                                @endforeach
+                                            @endif
+                                        </td>
+                                        <td class="align-middle">
+                                            <div class="justify-content-center align-content-end d-flex gap-2">
+                                                <a class="btn btn-info btn-sm"
+                                                    href="{{ route('users.show', $user->id) }}"><i
+                                                        class="fa-solid fa-list"></i> Show</a>
+                                                <a class="btn btn-primary btn-sm"
+                                                    href="{{ route('users.edit', $user->id) }}"><i
+                                                        class="fa-solid fa-pen-to-square"></i> Edit</a>
+                                                <div class="">
+                                                    <a type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                                        data-bs-target="#deleteUser{{ $user->id }}"><i
+                                                            class="fa-solid fa-trash me-2"></i>{{ __('Delete') }}</button>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @include('users.modal.delete')
+                                @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center align-middle">
+                                            <h3 class="p-3 fst-italic">
+                                                {{ __('No data found!') }}
+                                            </h3>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {!! $data->links('pagination::bootstrap-5') !!}
             </div>
         </div>
     </div>
-
-    @session('success')
-        <div class="alert alert-success" role="alert">
-            {{ $value }}
-        </div>
-    @endsession
-
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Roles</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($data as $key => $user)
-            <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>
-                    @if (!empty($user->getRoleNames()))
-                        @foreach ($user->getRoleNames() as $v)
-                            <label class="badge bg-success">{{ $v }}</label>
-                        @endforeach
-                    @endif
-                </td>
-                <td>
-                    <a class="btn btn-info btn-sm" href="{{ route('users.show', $user->id) }}"><i
-                            class="fa-solid fa-list"></i> Show</a>
-                    <a class="btn btn-primary btn-sm" href="{{ route('users.edit', $user->id) }}"><i
-                            class="fa-solid fa-pen-to-square"></i> Edit</a>
-                    <form method="POST" action="{{ route('users.destroy', $user->id) }}" style="display:inline">
-                        @csrf
-                        @method('DELETE')
-
-                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i>
-                            Delete</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-
-    {!! $data->links('pagination::bootstrap-5') !!}
-
 @endsection
---
+++ resources/views/users/modal/delete.blade.php
@@ -0,0 +1,33 @@
+<div class="modal fade" id="deleteUser{{ $user->id }}" tabindex="-1" aria-labelledby="deleteUser{{ $user->id }}"
+    aria-hidden="true" style="background-color: #80808090">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteUser{{ $user->id }}">
+                    {{ __('Delete reole') }}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                {{ __('Delete user') }}
+                <b>
+                    <i>
+                        <code>
+                            {{ $user->name }}
+                        </code>
+                        {{ __('?') }}
+                    </i>
+                </b>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn rounded-1 btn-sm btn-secondary px-4"
+                    data-bs-dismiss="modal">{{ __('Cancel') }}</button>
+                <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i>
+                        {{ __('Delete') }}</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
