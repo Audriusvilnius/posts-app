caab589a3c07daeae3d7ecc3826d4768643e1e49
Audrius <audrius.ivko@gmail.com>
2024-09-30 16:49:22 +0300

    Refactor page titles in app.blade.php and welcome.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+    <title>PIT-21-I-NT SD1/2</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
